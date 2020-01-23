567ad23a8cb067812480818ca599598de8ff8c2b
domreader <33025225+domreader@users.noreply.github.com>
2020-01-23 18:00:28 +0400

    Its Alive!
    Changes before 6pm this evening include
    
    -Figuring out how to serve static files in express to seperate the css from html
    -From looking at a couple tutorials being able to create schemas which can save data into then put into MongoDB database
    - Creating a form which pulls data and saves it
    - Able to view database and information

--- Menu Maestro/server.js
+++ Menu Maestro/server.js
@@ -2,10 +2,15 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const mongoose = require('mongoose')
-
+const express = require('express')
 // connecting to database
+app.use(express.static('css'))
+app.use(express.static('images'))
 
 const url = 'mongodb://127.0.0.1:27017/messages'
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect(url, {useNewUrlParser: true});
 
 const db = mongoose.connection
@@ -13,7 +18,27 @@ db.once('open',_=> {console.log('Database Connected :', url)})
 
 db.on('error', err => {console.error('connection error :',err)})
 
+var formSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  companyName: String,
+  emailAddress: String
+});
 
+var User = mongoose.model("User", formSchema);
+
+//db.users.find()pretty()
+
+app.post("/addname", (req, res) => {
+  var messages = new User(req.body);
+  messages.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+});
 // sending the html files to the server to display
 
 app.get('/index', function(req, res){
@@ -25,9 +50,10 @@ app.get('/menu', function(request, response) {
 app.get('/chat', function(request, response) {
   response.sendFile(__dirname + '/html/chat.html');
 });
-app.get('/contact', function(request, response) {
-  response.sendFile(__dirname + '/html/contact.html');
+app.get('/join', function(request, response) {
+  response.sendFile(__dirname + '/html/join.html');
 });
+
 
 // From the Socket IO tutorial however adding for user disconnect
 
